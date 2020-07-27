949f378106a084f49864050d1e4b8fa10b284d60
kicbar <bartlomiejkicior@gmail.com>
2020-07-27 22:41:59 +0200

    get data from url

--- wheatherapp.py
+++ wheatherapp.py
@@ -1,1 +1,15 @@
-print('Hello in wheatherApp!')
+from requests import get
+from json import loads
+
+def main():
+    url = 'https://danepubliczne.imgw.pl/api/data/synop'
+    response = get(url)
+
+    for row in loads(response.content):
+        #print(row)
+        #print(row['stacja'])
+        if row['stacja'] == 'Warszawa':
+            print(row)
+
+if __name__ == '__main__':
+    main()
