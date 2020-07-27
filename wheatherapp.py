f3ee9605016d9984644cc686f8f2f1719e60c718
kicbar <bartlomiejkicior@gmail.com>
2020-07-27 22:54:58 +0200

    add new feature

--- wheatherapp.py
+++ wheatherapp.py
@@ -1,15 +1,17 @@
 from requests import get
 from json import loads
 
+#set city for get data 
+CITIES = 'Warszawa'
+
 def main():
     url = 'https://danepubliczne.imgw.pl/api/data/synop'
     response = get(url)
 
     for row in loads(response.content):
-        #print(row)
-        #print(row['stacja'])
-        if row['stacja'] == 'Warszawa':
+        if row['stacja'] in CITIES:
             print(row)
 
 if __name__ == '__main__':
+    print('Hello in wheatherApp.')
     main()
